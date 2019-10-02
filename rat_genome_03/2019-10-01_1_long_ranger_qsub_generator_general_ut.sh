0c445bbd7fed73b9fb89b65dc5cdcd26c5eb6638
YANCHAO PAN <hanyou@acf-login5.acf.nics.utk.edu>
2019-10-02 16:33:13 -0400

    sync

--- rat_genome_03/2019-10-01_1_long_ranger_qsub_generator_general_ut.sh
+++ rat_genome_03/2019-10-01_1_long_ranger_qsub_generator_general_ut.sh
@@ -21,7 +21,7 @@ cd /lustre/haven/proj/UTHSC0013/UM/projects/rat_genome_03/long_ranger/$id
 longranger wgs --id=$id \
 --reference=/lustre/haven/proj/UTHSC0013/UM/projects/rat_genome_03/references/refdata-rn6.fa.concatenate \
 --fastqs=/lustre/haven/proj/UTHSC0013/UM/projects/rat_genome_03/sequences/$id/ \
---sample=${id}_1,${id}_2,${id}_3,${id}_4 \ 
+--sample=${id}_1,${id}_2,${id}_3,${id}_4 \
 --vcmode=gatk:/lustre/haven/proj/UTHSC0013/UM/apps/gatk_4.0.3.0/gatk-4.0.3.0/gatk-package-4.0.3.0-local.jar
 " >> $file_name
 
